0b3025d226f73c16cd9ca191e27a30c5202f616b
Lady-Byron <samantha-everett@outlook.com>
2026-01-03 23:39:21 -0500

    Update SerializesAndRestoresWrappedModelIdentifiers.php

--- src/Job/SerializesAndRestoresWrappedModelIdentifiers.php
+++ src/Job/SerializesAndRestoresWrappedModelIdentifiers.php
@@ -5,10 +5,8 @@ namespace ClarkWinkelmann\Scout\Job;
 use ClarkWinkelmann\Scout\ScoutModelWrapper;
 use Illuminate\Contracts\Database\ModelIdentifier;
 use Illuminate\Contracts\Queue\QueueableCollection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
-/**
- * Overrides the right methods to allow passing a collection of wrapped models to a queued job
- */
 trait SerializesAndRestoresWrappedModelIdentifiers
 {
     protected function getSerializedPropertyValue($value)
@@ -18,10 +16,7 @@ trait SerializesAndRestoresWrappedModelIdentifiers
 
             if ($first instanceof ScoutModelWrapper) {
                 return new ModelIdentifier(
-                // This is the reason we need to extend this whole method, because the class is otherwise not accessible
-                // We could also extend the RemoveableScoutCollection that would only help for one of the Scout jobs
                     get_class($first->getRealModel()),
-                    // This can be left as-is, we modified the method called in ScoutModelWrapper
                     $value->getQueueableIds(),
                     $value->getQueueableRelations(),
                     $value->getQueueableConnection()
@@ -29,14 +24,14 @@ trait SerializesAndRestoresWrappedModelIdentifiers
             }
         }
 
-        return parent::getRestoredPropertyValue($value);
+        // [FIX #11] 修复原版 bug：应该调用 getSerializedPropertyValue 而非 getRestoredPropertyValue
+        return parent::getSerializedPropertyValue($value);
     }
 
     protected function restoreCollection($value)
     {
-        return parent::restoreCollection($value)->map(function ($model) {
-            // The class stored is the real class, now we wrap it again so we don't have to modify every method in this job
-            return new ScoutModelWrapper($model);
-        });
+        $collection = parent::restoreCollection($value);
+        $wrapped = $collection->map(fn($m) => new ScoutModelWrapper($m));
+        return new EloquentCollection($wrapped->all());
     }
 }
